13ce0a80103ea4b7c8a452569f2802072a6ba71e
rAiner Gruber <rainer@ramaweb.de>
2026-03-15 21:37:17 +0100

    more refactoring

--- SvgLib/ElementBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SvgLib {
-    public abstract class ElementBase {
-        public abstract string Serialize(Canvas canvas);
-    }
-}
--- SvgLib/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SvgLib {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
